1abdb5c5b352c556b9b5bca0b3d3e7a52c4c7848
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-07-23 23:41:57 +0530

    Add files via upload

---
+++ Staircase_pattern.c
@@ -0,0 +1,19 @@
+//Staircase pattern
+
+void staircase(int n) {
+    for(int i=n;i>0;i--)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<i)
+            printf(" ");
+            else {
+            printf("#");
+            }
+        }
+        printf("\n");
+    }
+    
+
+}
+
