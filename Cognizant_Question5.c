5cc79b99f7be2f197f94fc60069f55767f3c2d9b
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-07-27 11:21:26 +0530

    Add files via upload

--- Cognizant_Question5.c
+++ Cognizant_Question5.c
@@ -9,7 +9,29 @@
 
 //In fibonacci series- the division of current/previous=1.6 (Golden Ratio)
 
-
+#include <stdio.h>
+int main()
+{
+	int arr[10],i,n,sum=0;
+	printf("Enter the size:");
+	scanf("%d",&n);
+	int total=((n+1)*(n+2))/2;
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+arr[i];
+	}
+	
+	int result=(total-sum);
+	printf("The missing element is:%d",result);
+	
+	
+	
+}
 
 
 
