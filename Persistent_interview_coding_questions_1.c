d632519442532658a51a5f592031aa985337d4ec
Ankita Prasad Gupta <64923045+ankitaapg17@users.noreply.github.com>
2021-08-12 00:04:44 +0530

    Add files via upload

---
+++ Persistent_interview_coding_questions_1.c
@@ -0,0 +1,71 @@
+//Level 1
+
+
+//Series AP
+//Series GP
+//Closest Number
+//Print the Kth Digit
+//Binary number to decimal number
+//Jumping Numbers
+//GCD of array
+//Factorial of a large number
+//Compute nPr
+//Compute nCr
+//Largest prime factor
+//Pair cube count
+//Find Nth root of M
+//
+//
+//Array:
+//
+//
+//Array (insert,search,delete)
+//Reverse the array
+//Rotate Array
+//Count of smaller elements
+//Remove duplicate elements from sorted Array
+//Merge 2 sorted arrays without using Extra space.
+//Find the "Kth" max and min element of an array 
+//Move all the negative elements to one side of the array 
+//Find the Union and Intersection of the two sorted arrays.
+//find all pairs on integer array whose sum is equal to given number
+//Rearrange the array in alternating positive and negative items with O(1) extra space
+//Find if there is any subarray with sum equal to 0
+//Find the triplet that sum to a given value
+//Median of 2 sorted arrays of equal size
+//Median of 2 sorted arrays of different size
+//
+//
+//
+//
+//String :
+//
+//
+//Reverse a String
+//Find Duplicate characters in a string
+//Print all Subsequences of a string.
+//Print all the permutations of the given string
+//Check for palindrome
+//Check for anagram
+//Anagram Palindrome
+//Title case conversion
+//longest string pallindrome
+//Sort the string
+//Merge two strings
+//Good or Bad string
+//Find the first repeated word in string.
+//Check if two given strings are isomorphic to each other
+//Reverse words in a given string
+//Anagram Search
+//First repeating character
+//First non-repeating character
+
+
+
+
+
+
+
+
+
+
